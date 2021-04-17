cefc069f48121c6943a4efadca127284be1bbc81
ScientificMEN <79857028+ScientificMEN@users.noreply.github.com>
2021-04-17 12:14:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+  background("yello");
+  text("EMAIL : armyscientific@gmail.com",100,200)
+  text("PASSWORD : scientific1234",100,250)
+}
