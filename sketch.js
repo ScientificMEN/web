24abb307e7954057ab1727e5d9f8a7539f6c9522
ScientificMEN <79857028+ScientificMEN@users.noreply.github.com>
2021-04-17 12:24:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,4 +6,5 @@ function draw() {
   background("yello");
   text("EMAIL : armyscientific@gmail.com",100,200)
   text("PASSWORD : scientific1234",100,250)
+  text("website name: server.pro",100,300)
 }
